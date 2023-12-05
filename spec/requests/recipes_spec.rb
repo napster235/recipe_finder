68f8dea8515256582261a7d404535a63dc7e1570
Dragomir, Tudor <dragomir.tuddor@gmail.com>
2023-12-05 21:16:29 +0200

    Add specs for ordering by ratings

--- spec/requests/recipes_spec.rb
+++ spec/requests/recipes_spec.rb
@@ -33,6 +33,63 @@ RSpec.describe 'recipes', type: :request do
         expect(response.body).to include('No recipes found')
       end
     end
+
+    context 'when ordering' do
+      let!(:recipe1) { create(:recipe, ratings: 1.0, ingredients: 'eggs, milk, salad') }
+      let!(:recipe2) { create(:recipe, ratings: 2.0, ingredients: 'eggs, cheese, chicken') }
+      let!(:recipe3) { create(:recipe, ratings: 3.0, ingredients: 'tuna, milk, cereals') }
+      let(:ordered_recipes) { assigns(:recipes) }
+
+      context 'by ratings in descending order' do
+        subject { get recipes_path, params: { order: 'DESC' } }
+
+        it 'orders the recipes by rating from high to low' do
+          subject
+
+          expect(ordered_recipes).to eq([recipe3, recipe2, recipe1])
+        end
+      end
+
+      context 'by ratings in ascending order' do
+        subject { get recipes_path, params: { order: 'ASC' } }
+
+        it 'orders the recipes by rating from low to high' do
+          subject
+
+          expect(ordered_recipes).to eq([recipe1, recipe2, recipe3])
+        end
+      end
+
+      context 'by ratings descending and searching by ingredient' do
+        subject { get recipes_path, params: { order: 'DESC', query: 'eggs' } }
+
+        it 'orders the recipes by rating from high to low' do
+          subject
+
+          expect(ordered_recipes).to eq([recipe2, recipe1])
+        end
+      end
+
+      context 'by ratings ascending and searching by ingredient' do
+        subject { get recipes_path, params: { order: 'ASC', query: 'eggs' } }
+
+        it 'orders the recipes by rating from low to high' do
+          subject
+
+          expect(ordered_recipes).to eq([recipe1, recipe2])
+        end
+      end
+
+      context 'when on index page, default order is DESC' do
+        subject { get recipes_path }
+
+        it 'always displays recipes by descending rating' do
+          subject
+
+          expect(ordered_recipes).to eq([recipe3, recipe2, recipe1])
+        end
+      end
+    end
   end
 
   describe 'GET #show' do
