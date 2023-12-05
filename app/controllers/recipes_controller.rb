461169dc282f545ba81a55e6524809d9a96e888e
Dragomir, Tudor <dragomir.tuddor@gmail.com>
2023-12-05 20:01:09 +0200

    Add ordering by ratings

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -12,10 +12,12 @@ class RecipesController < ApplicationController
   private
 
   def get_recipes_by_ingredients(query_param)
+    ordered_recipes = Recipe.order(params[:order] == 'ASC' ? 'ratings ASC' : 'ratings DESC')
+
     if query_param.present?
-      Recipe.search_by_ingredients(params[:query]).page(params[:page])
+      ordered_recipes.search_by_ingredients(params[:query]).page(params[:page])
     else
-      Recipe.random_recipes.page(params[:page])
+      ordered_recipes.random_recipes.page(params[:page])
     end
   end
 end
