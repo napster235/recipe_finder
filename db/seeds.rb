eec566283ce7fc62c12308b79b4e3fd9da496c59
Dragomir, Tudor <dragomir.tuddor@gmail.com>
2024-10-01 16:28:28 +0300

    Fix images to be displayed again

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 directory = File.dirname(__FILE__)
-file_location = File.join(directory, 'recipes-en.json')
+file_location = File.join(directory, 'output_recipes.json')
 recipe_list = JSON.parse(File.read(file_location))
 
 Recipe.insert_all(recipe_list)
